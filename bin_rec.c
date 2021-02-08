017203855596cdcb391e53a0a68c5b8c3236ccf7
Basil217 <basil217sut@gmail.com>
2021-02-08 17:41:22 +0300

    data_types_pptx

---
+++ bin_rec.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int bintoint(int x){
+    int z;
+    if (x!=0)
+        bintoint(x/2);    
+    else return z;
+    //printf("%d",x%2);
+    z=(z*10)+x%2;
+     printf("%d");
+    return z;
+}
+
+int main(int argc, const char** argv){
+    int x;
+    //int z = 0;
+    printf("%s","input an integer num:\n"); scanf("%d",&x);
+    printf("\n %d",bintoint(x));
+    return 0;
+}
--- test.c
+++ test.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, const char ** argv){
-    printf("Hi there!");
+    //printf("Hi there!");
+    
+int a = 12;
+printf("%d << 1 == %d\n", a, a << 1);
+printf("%d << 2 == %d\n", a, a << 2);
+printf("%d >> 1 == %d\n", a, a >> 1);
+printf("%d >> 2 == %d\n", a, a >> 2);
     return 0;
 }
