af87d2b73cad873d4a687f8a512abcb26279fcec
Basil217 <basil217sut@gmail.com>
2021-03-09 16:01:27 +0300

    B217

--- queue_arr.c
+++ queue_arr.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 #define T char
-#define SIZE 1000
+#define SIZE 10
 #define true 1 == 1
 #define false 1 != 1
 
@@ -11,35 +11,48 @@ typedef int bolean;
 T Queue[SIZE];
 int first = 0;
 int last = -1;
+int items = 0;
 
 bolean enqueue(T data){
-    if(last < SIZE){
-        Queue[++last]= data;
-        return true;
-    }else{
+    if(items==SIZE){
         printf("%s \n", "Queue is full");
         return false;
+    }
+    if(last == SIZE-1){
+        last = -1;
+        Queue[++last]= data;
+        items++;
+        return true;
     }
 }
 
 T dequeue(){
-    if(first<=last){
-        return Queue[first++];
-    }else{
+    if(items==0){
         printf("%s \n", "Queue is empty");
         return -1;
+    }else{
+        char tmp = Queue[first++];
+        //printf("%c ", tmp);
+        first %=SIZE;
+        items--;
+        return tmp; 
     }
 }
 
-int main(int argc, const char **argv){
-    enqueue('a');
-    enqueue('b');
-    enqueue('c');
-    enqueue('d');
-    enqueue('e');
-    enqueue('f');
-    while(last>=first){
-        printf("%c ", dequeue());
-    }
+int main(const int argc, const char **argv){
+    //int i=0;
+ //   while (i<10){
+        enqueue('a');
+        enqueue('b');
+        enqueue('c');
+        enqueue('d');
+        enqueue('e');
+        enqueue('f'); 
+        while(items>0){
+            printf("%c ", dequeue());
+        }
+        printf("\n");
+      //  i++;
+    
     return 0;
 }
---
+++ sorts/sorts_oop.cpp
@@ -0,0 +1,60 @@
+#include "iostream"
+#include "time.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+class sortArray{
+    int n;
+    int arr[n];
+    void swap(int *x, int *y){
+        int t=*x;
+        *x=*y;
+        *y=t;
+    }
+public:
+    sortArray(int* arr1, int N):n(N){
+        arr=arr1;
+    }
+    void fill_rand(){
+        srand(time(0));
+        for(int i=0; i<*n; ++i)
+            *(arr+i) = rand()%100; //a[i] = ... the same
+    }
+
+
+
+    void printArr(){
+        for(int i=0; i<*n; ++i)
+            printf("[%d] \t", *(arr+i));
+        printf("\n");
+    }
+
+/*
+    void inserts_1d(){
+        int tmp, pos;
+        for(int i=1;i<*n;++i){
+            tmp = arr[i];
+            pos = i-1;// 0 эл. считается отсортирован 
+                while ((pos)>=0 && arr[pos]>tmp){
+                    arr[pos+1]=arr[pos];
+                    pos--;
+                }
+            arr[pos+1] = tmp;      
+        }
+    }
+    */
+};
+
+int main(int argc, const char *_argv[]){
+     int N=0;  cin>>N;
+     int* arr;
+     sortArray done(arr,N);
+     done.fill_rand();
+     done.printArr();
+     //done.inserts_1d();
+
+}
+
+
