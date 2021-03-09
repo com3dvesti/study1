b9a3018baec5f827c70a63dfae4d4926cedf8821
Basil android_8 <com3.dvesti@bk.ru>
2021-03-09 16:17:04 +0300

    power2

---
+++ power.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using std::cout;
+
+class Power{
+    float x, p;
+public:
+    Power():x(0),p(0){
+    }
+    ~Power(){
+    } 
+    void set(float xf, float pf){
+    x=xf; p=pf;
+    }
+    float calculate(){
+        float xi=x, x_pow_p=x; 
+         
+        //int pff=(int)p;
+        //cout << pff <<"\n";
+        if(p==0){
+                x_pow_p=1;
+                cout << x_pow_p<<"\n";
+                return x_pow_p;
+       } else {
+            for(int i=1;i<p;i++){  
+                x_pow_p*=xi;
+            }  
+          cout << x_pow_p;
+          return x_pow_p;
+         }
+    } 
+};
+    
+int main(){
+	    Power Be;
+    Be.set(2,10);
+    Be.calculate();
+	return 0;
+}
+    
