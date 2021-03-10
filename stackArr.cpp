752cde94c7794fe84c79d5f10e7b1b01eac32d03
Basil android_8 <com3.dvesti@bk.ru>
2021-03-10 14:11:42 +0300

    stack from android

---
+++ stackArr.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Stack{
+    int arrSta[9];
+    int head;
+public:
+    Stack():head(-1){};
+    void reset(){
+        for(int i=0;i<=head;i++)
+            arrSta[i]=0;
+        head =-1;
+    }
+    bool push(int data){
+        if (head<(sizeof(arrSta)/sizeof(int))){
+            arrSta[head++]=data;
+            return true;
+        }else{
+            cout<<"Stack is overflow" ;
+            return false;
+        } 
+    }
+    int pop(){
+        if (head != -1){
+        return arrSta[head--];
+    } 
+    void print(){
+        if(head ==-1){
+            cout << "() \n";
+        }else{
+            cout << "(";
+            for(int i=0;i<=head;i++)
+                cout<<" "<< arrSta[i]<<" ";
+            cout << ") \n";
+    }
+};
+
+int main(int argc, char** argv){
+    Stack stack;
+    stack.print();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.print();
+    
+    stack.pop();
+    stack.print();
+    return 0;
+}
+
