a680de588d12789b68d68dcbc6ed09fbc87a389f
Basil Sut <1vasilisk12@gmail.com>
2021-03-29 08:06:00 +0300

    exemple of cmake

---
+++ exemple/src/program_gb.cpp
@@ -0,0 +1,10 @@
+#include "program_gb.h"
+using namespace std;
+
+int main(int argc, char** args)
+{
+    const int SUCCESS=0;
+    cout << IT_WORKS << endl;
+    cout << QUESTION << endl;
+    return SUCCESS;
+}
